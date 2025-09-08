92427318a21f96d4207f40f2aaf0be152a2a3903
Andyish-tech <ishmukazi@gmail.com>
2025-09-08 13:11:56 +0200

    Functins in python

--- Intro/List/list.py
+++ Intro/List/list.py
@@ -1,3 +1,5 @@
+# List is similar to (array) in javascript
+
 fruit = ["apple","banana","cherry"]
 x,y,z = fruit
 print(x) #which will be apple 
---
+++ Intro/functiond/def.py
@@ -0,0 +1,6 @@
+#we use def for decraling function
+
+def mynames():
+x= "ISHIMWE Andy"
+mynames()
+print(mynames())
