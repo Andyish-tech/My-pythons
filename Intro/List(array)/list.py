dddc4c4817ffd2fe9170d7264191e18601eac8c2
Andyish-tech <ishmukazi@gmail.com>
2025-09-08 12:37:59 +0200

    Day 1 2025-2026

---
+++ Intro/List(array)/list.py
@@ -0,0 +1,3 @@
+fruit = ["apple","banana","cherry"]
+x,y,z = fruit
+print(x) #which will be apple 
---
+++ Intro/variables/variable.py
@@ -0,0 +1,15 @@
+# Decraling variable you use only variable name
+#example
+ 
+ x = 10 
+
+ #then print
+
+ print(x)
+
+ #For multi variable you need to make sure that number of variable is eqaul to the number of values 
+ #example
+
+ x, y, z ="Avocado","banana","berry"
+
+ #you can print each according to Its position
