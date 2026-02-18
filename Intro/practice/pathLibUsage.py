53150f93e8db221ebcce76d2e1955fcdddbefd25
Andyish-tech <ishmukazi@gmail.com>
2026-02-18 16:00:35 +0200

    pathlib write

--- Intro/practice/pathLibUsage.py
+++ Intro/practice/pathLibUsage.py
@@ -18,3 +18,21 @@ try:
     print(f"folder {new_folder} created")
 except FileExistsError:
     print(f"folder {new_folder} already exists")
+    
+print("exists?: ", new_folder.exists())
+print("is directory?: ", new_folder.is_dir())
+print("is file?: ", new_folder.is_file(),"\n")
+
+# Nested folders
+
+nested = Path('david/wivine/gisubizo')
+nested.mkdir(parents= True, exist_ok=True)
+print("nested folder ", {nested})
+
+file_path = Path('david/wivine.txt')
+file_path.write_text("I love you David \n  'wivine'")
+print(f"file{file_path} created successfully")
+
+cartoon = file_path.read_text()
+print(" \n ----- cartoon content -----")
+print(cartoon)
