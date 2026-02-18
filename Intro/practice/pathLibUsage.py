71e026112590689cf794432955bd3cee1e0ccb2b
Andyish-tech <ishmukazi@gmail.com>
2026-02-18 15:35:57 +0200

    create folder using pathlib

--- Intro/practice/pathLibUsage.py
+++ Intro/practice/pathLibUsage.py
@@ -1,4 +1,20 @@
 from pathlib import Path
 
 current = Path('.')
-print('The current directory is: ', current)
+print('The current directory is: ', current.resolve(),'\n \n')
+
+# to see everything in the current dir
+
+print("\n ---- content of the current directory -----")
+for item in current.iterdir():
+    print(item.name, '| is file: ', item.is_file(), '| is directory: ', item.is_dir() )
+    
+    
+# create a folder
+
+new_folder = Path('andy')
+try:
+    new_folder.mkdir()
+    print(f"folder {new_folder} created")
+except FileExistsError:
+    print(f"folder {new_folder} already exists")
