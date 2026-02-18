60933ecbec930264b5624e3098a7044546907310
Andyish-tech <ishmukazi@gmail.com>
2026-02-18 12:51:23 +0200

    # create a folder and write inside

--- Intro/practice/osexample.py
+++ Intro/practice/osexample.py
@@ -1,11 +1,23 @@
-import os
+import os as ish
 
 # create a folder
 
 try:
-    os.mkdir('example')
+    ish.mkdir('example')
     print('Folder is created succesfully')
 except FileExistsError:
     print("Folder you want to create alread exists")
     
-print(os.listdir('.'))
+# print(ish.listdir('.'))
+
+file_path= 'hello.txt'
+
+try:
+    with open(file_path, 'w') as file:
+        file.write('Hello level 5 pionerr. \n')
+        file.write('file handling is not hard as you said')
+    print(f"File '{file_path}'create successfully")
+except IOError as err:
+    print(f"something went wrong while creating file: {err}")
+    
+print(ish.path.exists(file_path))    
