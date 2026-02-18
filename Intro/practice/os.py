e1d3ac44b97dcabc2e55dec827ec0ddf83c8bf95
Andyish-tech <ishmukazi@gmail.com>
2026-02-18 12:19:56 +0200

    Information on the file or folder

--- Intro/practice/os.py
+++ Intro/practice/os.py
@@ -8,3 +8,9 @@ print(os.listdir('.'))
 #Check existance
 
 print(os.path.exists('os.py'))
+
+#Check Information on the file or folder
+             # check if is file
+print(os.path.isfile('os.py'))
+             # check if is folder
+print(os.path.isdir('os.py'))
