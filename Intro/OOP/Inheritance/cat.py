3d5bc1769ce7c60c1e0480b69fb3e0bdd9507a6c
Andyish-tech <ishmukazi@gmail.com>
2026-02-20 12:22:08 +0200

    Update cat.py

--- Intro/OOP/Inheritance/cat.py
+++ Intro/OOP/Inheritance/cat.py
@@ -7,7 +7,7 @@ class Cat(Animal):
         return "Meow"
 cat = Cat()
 
-print('\n ------ Cat Class')
+print('\n ------ Cat Class ------')
 print('The cat speaks ', cat.Speak(),'\n')
 
 class Armand:
@@ -21,15 +21,29 @@ class Gisubizo:
     def courts(self):
         return "court"
 
+# Multiple inheritance
 class David(Armand, Benjamin, Gisubizo):
     pass
 
 david = David()
 
-print("\n ---Badman David ability")
+print("\n ---Badman David ability ----")
 print("David can", david.walk())
 print("David can", david.speak())
 print("David can", david.courts())
 
+# multiple level inheritance
+
+class Trent(David):
+    def eat(self):
+        return "Eat"
+    
+trent =Trent()
+
+print("\n ---Trent's ability ----")
+print("Trent can", trent.walk())
+print("Trent can", trent.speak())
+print("Trent can", trent.courts())
+print("Trent can", trent.eat())
 
 
