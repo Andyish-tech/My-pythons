c2d906d49605ca3fe19287e2fd35bc66c77e0d7b
Andyish-tech <ishmukazi@gmail.com>
2026-02-20 12:11:27 +0200

    Update cat.py

--- Intro/OOP/Inheritance/cat.py
+++ Intro/OOP/Inheritance/cat.py
@@ -8,4 +8,28 @@ class Cat(Animal):
 cat = Cat()
 
 print('\n ------ Cat Class')
-print('The cat speaks ', cat.Speak())
+print('The cat speaks ', cat.Speak(),'\n')
+
+class Armand:
+    def walk(self):
+        return"walk"
+
+class Benjamin:
+    def speak(self):
+        return "Speak"
+class Gisubizo:
+    def courts(self):
+        return "court"
+
+class David(Armand, Benjamin, Gisubizo):
+    pass
+
+david = David()
+
+print("\n ---Badman David ability")
+print("David can", david.walk())
+print("David can", david.speak())
+print("David can", david.courts())
+
+
+
